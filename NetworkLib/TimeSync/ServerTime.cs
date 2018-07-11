e089494dd83590777022034562e4c59d9a440a1f
petar.nikolov <petar.nikolov@resolvesys.com>
2018-07-11 12:51:03 +0300

    Time info sync update

--- NetworkLib/TimeSync/ServerTime.cs
+++ NetworkLib/TimeSync/ServerTime.cs
@@ -28,6 +28,73 @@ namespace Network.TimeSync
         [DllImport("kernel32.dll", EntryPoint = "SetSystemTime", SetLastError = true)]
         public extern static bool Win32SetSystemTime(ref SystemTime sysTime);
 
+        [PreserveSig]
+        [DllImport("advapi32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto)]
+        public static extern int AdjustTokenPrivileges(int tokenhandle, int disableprivs,
+                                                       [MarshalAs(UnmanagedType.Struct)] ref TOKEN_PRIVILEGES Newstate,
+                                                       int bufferlength, int PreivousState, int Returnlength);
+
+        [PreserveSig]
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto)]
+        public static extern int GetCurrentProcess();
+
+        [PreserveSig]
+        [DllImport("advapi32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto)]
+        public static extern int LookupPrivilegeValue(string lpsystemname, string lpname,
+                                                      [MarshalAs(UnmanagedType.Struct)] ref LUID lpLuid);
+
+        [PreserveSig]
+        [DllImport("advapi32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto)]
+        public static extern int OpenProcessToken(int ProcessHandle, int DesiredAccess, ref int tokenhandle);
+
+        [PreserveSig]
+        [DllImport("advapi32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto,
+            SetLastError = true)]
+        public static extern int RegLoadKey(uint hKey, string lpSubKey, string lpFile);
+
+        [PreserveSig]
+        [DllImport("advapi32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto,
+            SetLastError = true)]
+        public static extern int RegUnLoadKey(uint hKey, string lpSubKey);
+
+        #region Nested type: LUID
+
+        public struct LUID
+        {
+            public int HighPart;
+            public int LowPart;
+        }
+
+        #endregion
+
+        #region Nested type: TOKEN_PRIVILEGES
+
+        public struct TOKEN_PRIVILEGES
+        {
+            public int Attributes;
+            public LUID Luid;
+            public int PrivilegeCount;
+        }
+
+        #endregion
+        private static void SetPrivilege(string privilege)
+        {
+            int i1 = 0;
+            var luid = new LUID();
+            var token_PRIVILEGES = new TOKEN_PRIVILEGES();
+            int i2 = OpenProcessToken(GetCurrentProcess(), 40, ref i1);
+            if (i2 == 0)
+                throw new Exception("OpenProcessToken For Privilege <" + privilege + "> Failed");
+            i2 = LookupPrivilegeValue(null, privilege, ref luid);
+            if (i2 == 0)
+                throw new Exception("LookupPrivilegeValue For Privilege <" + privilege + "> Failed");
+            token_PRIVILEGES.PrivilegeCount = 1;
+            token_PRIVILEGES.Attributes = 2;
+            token_PRIVILEGES.Luid = luid;
+            i2 = AdjustTokenPrivileges(i1, 0, ref token_PRIVILEGES, 1024, 0, 0);
+            if (i2 == 0)
+                throw new Exception("AdjustTokenPrivileges For Privilege <" + privilege + "> Failed");
+        }
 
         public static void Get(IPEndPoint localEndPoint, IPEndPoint targetEndPoint)
         {
@@ -39,6 +106,7 @@ namespace Network.TimeSync
 
         public static void Set(DateTime dateTime)
         {
+            SetPrivilege("SeSystemtimePrivilege");
             var st = new SystemTime();
             st.Year = (ushort)dateTime.Year;
             st.Month = (ushort)dateTime.Month;
